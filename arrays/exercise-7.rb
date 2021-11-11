c6b107ed71b3d54c72a807e45a362e864890b8ef
Anthony <knoxbartl@gmail.com>
2021-11-11 15:29:31 -0600

    Arays exercises added

---
+++ arrays/exercise-7.rb
@@ -0,0 +1,3 @@
+my_array = ["Michael", "Jim", "Dwight", "Pam", "Creed", "Stanley", "Angela", "Oscar"]
+
+my_array.each_with_index { |value, index| puts "#{index}: #{value}"}
---
+++ arrays/exercise-8.rb
@@ -0,0 +1,7 @@
+my_array = [1, 3, 5, 7, 9, 11]
+new_array = []
+
+new_array = my_array.map { |value| value + 2 }
+
+p my_array
+p new_array
